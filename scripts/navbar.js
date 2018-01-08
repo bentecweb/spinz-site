2f53523c6a5a3824e73ffe22ab92acf72502afda
Unknown <ben@bentec.co.uk>
2018-01-08 17:45:59 +0000

    Fix images

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -4,12 +4,13 @@ $(window).scroll(function () {
         navbar.classList.remove('navbar-black');
         navbar.classList.add('navbar-white');
         document.getElementById("navlogo").src = "images/Logo.svg";
+        document.getElementById("navbar-toggler-icon").style.backgroundImage="url('images/navbar-toggler-icon-black.svg')"; 
     }
     else {
         navbar.classList.remove('navbar-white');
         navbar.classList.add('navbar-black');
         document.getElementById("navlogo").src="images/LogoI.svg";
-        document.getElementById("navbar-toggler-icon").style.backgroundImage="url('../images/navbar-toggler-icon-white.svg')"; 
+        document.getElementById("navbar-toggler-icon").style.backgroundImage="url('images/navbar-toggler-icon-white.svg')"; 
     }
 });
 
